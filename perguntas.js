1dd7fc07eb4424bcfbbd78b4fc9256dccffa5d5b
hebertefabricio <148267252+hebertefabricio@users.noreply.github.com>
2024-10-02 19:16:07 +0000

    cartão

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+   'pogramação',
+   'O que e JavaScript?',
+   'O Javascript é uma linguagem de progamaçaão'
+)
+
+criaCartao(
+    'Geografia',
+    'qual a capital da França?',
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
